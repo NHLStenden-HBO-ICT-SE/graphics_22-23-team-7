cc73119b089d93d0c12adfeb46b49e22f49a6c5d
BardoBardo <nigelhois47@gmail.com>
2022-09-29 00:02:29 +0200

    added getClosestShape which loops over all shapes in scene to find the closest and changed sphere to general shape

--- src/classes/Scene.java
+++ src/classes/Scene.java
@@ -4,40 +4,54 @@ import classes.math.Point3D;
 import classes.math.Ray;
 import classes.math.Vector3D;
 import classes.objects.IntersectionHandler;
-import classes.objects.Sphere;
 import classes.view.Camera;
 import classes.view.Light;
+import interfaces.objects.Shape;
 
 import java.awt.*;
 
 import static classes.math.GenericMath.clamp;
 
 public class Scene {
-    private Camera camera;
-    private Sphere[] spheres; //TODO: change to generic object later
-    private Light[] lights;
+    private final Camera camera;
+    private final Shape[] shapes;
+    private final Light[] lights;
 
-    public Scene(Camera camera, Sphere[] spheres, Light[] lights) {
+    public Scene(Camera camera, Shape[] shapes, Light[] lights) {
         this.camera = camera;
-        this.spheres = spheres;
+        this.shapes = shapes;
         this.lights = lights;
     }
 
     public Color calculatePixel(Ray ray) {
-        for (Sphere sphere : spheres) {
-            IntersectionHandler intersectionHandler = sphere.intersection(ray);
 
-            if (!intersectionHandler.isIntersected()) return new Color(255, 255, 255);
+        var closestShape = getClosestShape(ray);
 
-            Point3D intersectionPoint = ray.getOrigin().addVector(ray.getDirection().multiply(intersectionHandler.getLength()));
+        if (!closestShape.isIntersected()) return Color.black;
 
-            double intensity = calculateIntensity(intersectionPoint);
+        Point3D intersectionPoint = ray.getOrigin().addVector(ray.getDirection().multiply(closestShape.getLength()));
 
-            return new Color(clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255));
-        }
-        return new Color(0, 0, 0); //TODO: try catch
+        double intensity = calculateIntensity(intersectionPoint);
+
+        return new Color(clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255));
     }
 
+    private IntersectionHandler getClosestShape(Ray ray) {
+        var closestShape = new IntersectionHandler(false, Double.MAX_VALUE);
+
+        for (Shape shape : shapes) {
+
+            IntersectionHandler currentShape = shape.intersection(ray);
+
+            //go to next shape if ray doesn't hit object
+            if (!currentShape.isIntersected()) continue;
+
+            //if current shape length is smaller than closest shape length then set closest shape to current shape
+            if (currentShape.getLength() < closestShape.getLength()) closestShape = currentShape;
+
+        }
+        return closestShape;
+    }
 
     private double calculateIntensity(Point3D origin) {
         double intensity = 0;
@@ -47,11 +61,13 @@ public class Scene {
 
             Ray ray = new Ray(origin, direction, origin.distance(light.getPosition()));
 
-            for (Sphere sphere : spheres) {
+            intensity = 0;
 
-                if (sphere.intersection(ray).isIntersected()) continue;
+            for (Shape shape : shapes) {
 
-                Vector3D N = sphere.getCenter().getVector(origin).normalize();
+                if (shape.intersection(ray).isIntersected()) continue;
+
+                Vector3D N = shape.getOrigin().getVector(origin).normalize();
 
                 double angle = N.dot(direction);
 

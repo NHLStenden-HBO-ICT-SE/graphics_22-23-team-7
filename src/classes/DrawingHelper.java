6be9946b9bdc75e815047833eb49d9545f1c4453
xterbione <bryan_K_@hotmail.com>
2022-09-28 12:59:41 +0200

    added shading.

--- src/classes/DrawingHelper.java
+++ src/classes/DrawingHelper.java
@@ -1,12 +1,16 @@
 package classes;
 
+import classes.math.Point3D;
 import classes.math.Ray;
-import classes.objects.Shape;
+import classes.math.Vector3D;
+import classes.objects.IntersectionHandler;
 import classes.objects.Sphere;
 import classes.view.Camera;
 import classes.view.Light;
 
 import java.awt.*;
+
+import static classes.math.GenericMath.*;
 
 public class DrawingHelper {
     private final MainWindow window;
@@ -31,16 +35,36 @@ public class DrawingHelper {
             for (int i = 0; i < this.window.getWidth(); i++) {
 
                 Ray ray = camera.makeRay((double) i / this.window.getWidth(), (double) j / this.window.getHeight());
-                Shape shape = sphere.intersection(ray);
+                IntersectionHandler intersectionHandler = sphere.intersection(ray);
 
                 //check if ray intersects with sphere
-                if (shape.isIntersected()) {
+                if (intersectionHandler.isIntersected()) {
 
-                    double intensity = light.calculateIntensity(shape.getPoint());
+                    //get point of intersection of sphere
+                    Point3D intersectionPoint = ray.getOrigin().addVector(ray.getDirection().multiply(intersectionHandler.getLength()));
 
-                    //light on a black shape
+                    //get direction from sphere to light
+                    Vector3D directionslRay = intersectionPoint.getVector(light.getPosition());
+
+                    //distance between light and sphere
+                    double slDistance = intersectionPoint.distance(light.getPosition());
+
+                    //create new ray from intersection to light
+                    Ray slRay = new Ray(intersectionPoint, directionslRay, slDistance);
+
+                    //get intersection from sphere and light
+                    var slIntersectionHandler = sphere.intersection(slRay);
+
+                    //init intensity
+                    double intensity = 0;
+
+                    if (!slIntersectionHandler.isIntersected())
+                        intensity = light.calculateIntensity(slDistance);
+
+
+                    //light on a black intersectionHandler
                     //new color should be moved to light
-                    this.window.Draw(i, j, new Color((int) (255 * intensity), (int) (255 * intensity), (int) (255 * intensity)));
+                    this.window.Draw(i, j, new Color(clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255), clamp((int) (255 * intensity), 0, 255)));
                 }
             }
         }

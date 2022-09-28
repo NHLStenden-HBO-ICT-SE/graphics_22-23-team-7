7d08d32955c50c6bcb684cb5d5394077e56589b3
BardoBardo <nigelhois47@gmail.com>
2022-09-29 00:02:47 +0200

    cleanup

--- src/classes/math/Ray.java
+++ src/classes/math/Ray.java
@@ -9,13 +9,13 @@ public class Ray {
     // Constructors
     //*****************************
 
-    public Ray(Vector3D direction, float distance) {
+    public Ray(Vector3D direction, double distance) {
         this(new Point3D(), direction, distance);
     }
 
-    public Ray(Point3D origin, Vector3D direction, float distance) {
+    public Ray(Point3D origin, Vector3D direction, double distance) {
         this.origin = new Point3D(origin);
-        this.direction = new Vector3D(direction); //TODO: normalize?
+        this.direction = new Vector3D(direction).normalize();
         this.t = distance;
     }
 

857e69f801dd7440328998e290a341a1596380c2
BardoBardo <nigelhois47@gmail.com>
2022-09-28 23:57:52 +0200

    point -> length to make code faster, you can calculate point yourself if you'd like

--- src/classes/objects/IntersectionHandler.java
+++ src/classes/objects/IntersectionHandler.java
@@ -1,10 +1,8 @@
 package classes.objects;
 
-import classes.math.Point3D;
-
-public class Shape {
-    private Point3D point;
+public class IntersectionHandler {
     private final boolean intersects;
+    private final double length; //length
     private double intensity; //might not need this, have to check later
 
     //*****************************
@@ -14,17 +12,24 @@ public class Shape {
     /**
      * @param intersects
      */
-    public Shape(boolean intersects) {
-        this(intersects, null); //maybe change to new Point3D()
+    public IntersectionHandler(boolean intersects) {
+        this(intersects, 0);
     }
 
     /**
      * @param intersects
-     * @param point
      */
-    public Shape(boolean intersects, Point3D point) {
+    public IntersectionHandler(double length) {
+        this(true, length);
+    }
+
+    /**
+     * @param intersects
+     * @param length
+     */
+    public IntersectionHandler(boolean intersects, double length) {
         this.intersects = intersects;
-        this.point = point;
+        this.length = length - 0.00001;
     }
 
     //*****************************
@@ -33,6 +38,7 @@ public class Shape {
 
     /**
      * gets intensity of the light hitting the shape
+     *
      * @return
      */
     public double getIntensity() {
@@ -41,14 +47,16 @@ public class Shape {
 
     /**
      * gets point of shape
+     *
      * @return
      */
-    public Point3D getPoint() {
-        return point;
+    public double getLength() {
+        return length;
     }
 
     /**
      * returns true if ray intersects with shape
+     *
      * @return
      */
     public boolean isIntersected() {

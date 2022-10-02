7d6680c49dfdd87efda8bd232c061a4497270ee6
BardoBardo <nigelhois47@gmail.com>
2022-10-02 23:03:26 +0200

    demo orbit

--- src/App.java
+++ src/App.java
@@ -13,9 +13,9 @@ public class App {
         //
         // Background
         //
-        var posB = new Point3D(-2.5, 2, 42); //position
+        var posB = new Point3D(0, 0, 32); //position
 
-        var sphereB = new Sphere(posB, 16);
+        var sphereB = new Sphere(posB, 13);
 
         //mass
         double massB = 0; //massless object
@@ -26,46 +26,90 @@ public class App {
         //
         // small moon
         //
-        var posM = new Point3D(-3, 0, 3); //position
+        var posM = new Point3D(-5, 0, 7); //position
 
-        var sphereM = new Sphere(posM, 0.3);
+        var sphereM = new Sphere(posM, 0.4);
 
         //mass
-        double massM = 0; //massless object
+        double massM = 1; //massless object
 
         //velocity
-        var velM = new Vector3D(0.1, 0.1, 0);
+        var velM = new Vector3D(0, 0.14988, 0.13);
+
+        //
+        // small moon2
+        //
+        var posM2 = new Point3D(0, 0, 7); //position
+
+        var sphereM2 = new Sphere(posM2, 0.4);
+
+        //mass
+        double massM2 = 1; //massless object
+
+        //velocity
+        var velM2 = new Vector3D(-0.05, -0.1, 0);
 
 
         //
         // big moon
         //
-        var posBigM = new Point3D(0, 0, 10); //position
+        var posBigM = new Point3D(-2, 0, 5); //position
 
-        var sphereBigM = new Sphere(posBigM, 2);
+        var sphereBigM = new Sphere(posBigM, 0.4);
 
         //mass
-        double massBigM = 2;
+        double massBigM = 1;
 
         //velocity
-        var velBigM = new Vector3D(0, 0, 0);
+        var velBigM = new Vector3D(0.2, 0.1, 0);
+
+        //
+        // big moon
+        //
+        var posBigM2 = new Point3D(2, -5, 3); //position
+
+        var sphereBigM2 = new Sphere(posBigM2, 0.4);
+
+        //mass
+        double massBigM2 = 1;
+
+        //velocity
+        var velBigM2 = new Vector3D(0.2, 0.1, 0);
+
+        //
+        // sun
+        //
+        var posS = new Point3D(0, -5, 0); //position
+
+        var sphereS = new Sphere(posS, 0.4);
+
+        //mass
+        double massS = 1;
+
+        //velocity
+        var velS = new Vector3D(-0.05, -0.05, 0);
 
         //
         // planets
         //
         Planet[] planets = {
                 new Planet(massM, velM, sphereM),
+                new Planet(massM2, velM2, sphereM2),
                 new Planet(massBigM, velBigM, sphereBigM),
+                new Planet(massBigM2, velBigM2, sphereBigM2),
+                new Planet(massS, velS, sphereS),
                 new Planet(massB, velB, sphereB, true),
         };
 
         //lights
-        Point3D originL = new Point3D(-4, -3, -1.3);
-        Point3D originL2 = new Point3D(3, 3, 10.5);
+        Point3D originL = new Point3D(-4, -3, -5);
+        Point3D originL2 = new Point3D(5, 10, -10);
+        Point3D originL3 = new Point3D(-4, 0, 3);
         Light[] lights = {
 
-                new Light(69, originL), //light1
-                new Light(12, originL2) //light2
+                new Light(30, originL), //light1
+                new Light(50, originL2), //light2
+                new Light(20, originL3) //light3
         };
 
         //init drawinghelper
